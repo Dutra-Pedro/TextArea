1964871374136d92890c71257f26b0120d7afdcd
logonrmlocal <pepeu@gmail.com>
2023-04-13 11:45:44 -0300

    Specify commit message

--- src/br/com/TextAreaFrame/TextAreaFrame.java
+++ src/br/com/TextAreaFrame/TextAreaFrame.java
@@ -29,7 +29,7 @@ public class TextAreaFrame extends JFrame{
 
     public TextAreaFrame(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100,100,350,200);
+        setBounds(100,100,500,200);
 
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5,5,5,5));
